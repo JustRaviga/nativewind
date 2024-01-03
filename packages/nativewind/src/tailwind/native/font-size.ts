0402f2e2fb831e32f598c47ee4914b1937e907e8
Andy Coates <andy@acoates.co.uk>
2024-01-03 14:27:40 +0000

    Fix font size object syntax
    See https://github.com/marklawlor/nativewind/issues/404#issuecomment-1663950429

--- packages/nativewind/src/tailwind/native/font-size.ts
+++ packages/nativewind/src/tailwind/native/font-size.ts
@@ -1,5 +1,7 @@
-import { PluginAPI } from "tailwindcss/types/config";
-import { isPlainObject } from "./utils";
+import {PluginAPI} from "tailwindcss/types/config";
+import {isPlainObject} from "./utils";
+
+const toHyphen = (string: string) => string.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
 
 /**
  * This "fixes" the fontSize plugin to calculate relative lineHeight's
@@ -9,27 +11,31 @@ import { isPlainObject } from "./utils";
  * { lineHeight: 1, fontSize: 12 } -> { lineHeight: 12, fontSize 12}
  * { lineHeight: 1px, fontSize: 12 } -> { lineHeight: 1px, fontSize 12}
  */
-export const fontSize = ({ matchUtilities, theme }: PluginAPI) => {
+export const fontSize = ({matchUtilities, theme}: PluginAPI) => {
   matchUtilities(
     {
       text: (value: unknown) => {
-        const [fontSize, options] = Array.isArray(value) ? value : [value];
-        const { lineHeight, letterSpacing } = isPlainObject(options)
-          ? options
-          : { lineHeight: options, letterSpacing: undefined };
+        const [_fontSize, _options] = Array.isArray(value) ? value : [value];
+        const options = isPlainObject(_options)
+          ? {..._options, fontSize: _fontSize}
+          : {lineHeight: _options, letterSpacing: undefined, fontSize: _fontSize};
+        const lineHeight = options.lineHeight;
 
         return {
-          "font-size": fontSize,
+          ...Object.fromEntries(
+            Object.entries(options)
+              .map(([key, value]) => [
+                toHyphen(key),
+                value])
+              .filter(([, value]) => value !== undefined)
+          ),
           ...(lineHeight === undefined
             ? {}
             : {
-                "line-height": lineHeight.endsWith("px")
-                  ? lineHeight
-                  : `${Number.parseFloat(fontSize) * lineHeight}px`,
-              }),
-          ...(letterSpacing === undefined
-            ? {}
-            : { "letter-spacing": letterSpacing }),
+              "line-height": lineHeight.endsWith("px")
+                ? lineHeight
+                : `${Number.parseFloat(_fontSize) * lineHeight}px`,
+            }),
         };
       },
     },
